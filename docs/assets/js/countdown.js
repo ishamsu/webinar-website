196e1f53844ee4723545d800d06902122eae0847
ishamsu <shamsu1997@gmail.com>
2020-04-15 13:51:25 +0530

    file moved to /docs

--- docs/assets/js/countdown.js
+++ docs/assets/js/countdown.js
@@ -1,5 +1,5 @@
 const year = new Date().getFullYear();
-const twentysevenJuly = new Date(year, 4, 20).getTime();
+const twentysevenJuly = new Date(year, 3, 15).getTime();
 console.log(twentysevenJuly);
 
 // countdown
@@ -12,6 +12,7 @@ let timer = setInterval(function() {
     const diff = twentysevenJuly - today;
 
     // math
+    let d1 = 0;
     let days = Math.floor(diff / (1000 * 60 * 60 * 24));
     let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
@@ -20,13 +21,13 @@ let timer = setInterval(function() {
     // display
     document.getElementById("timer").innerHTML =
         "<div style=\"font-size:10px\">next weninar will start within</div><br><div class=\"days\"> \
-  <div class=\"numbers\">" + days + "</div>days</div> \
+  <div class=\"numbers\">" + d1 + "</div>days</div> \
 <div class=\"hours\"> \
-  <div class=\"numbers\">" + hours + "</div>hours</div> \
+  <div class=\"numbers\">" + d1 + "</div>hours</div> \
 <div class=\"minutes\"> \
-  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+  <div class=\"numbers\">" + d1 + "</div>minutes</div> \
 <div class=\"seconds\"> \
-  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+  <div class=\"numbers\">" + d1 + "</div>seconds</div> \
 </div>";
 
 }, 1000);
